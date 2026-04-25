7d270603976c2ef2ac1e4a64340c369f0e5055e7
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-04-25 16:22:21 +0900

    34限の授業内容

---
+++ doyo12_2/doyo12_2/doyo12_2.cpp
@@ -0,0 +1,31 @@
+﻿// doyo12_2.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include<stdio.h>
+
+//	answe　をコンソールに表示する関数 f を定義する
+// 引数    : int 型の値を受け取る
+// 戻り値  : 引数の二つの値を足した値
+void f() {
+	static int answer = 0;
+	answer = answer + 1;
+	printf("関数ｆが呼び出されました。　answer は[%d] です\n", answer);
+	return 0;
+	
+}
+// メイン関数
+int main(){
+
+	// 関数 f を使う
+	 f(1,3);
+	 f();
+	return 0;
+}
+//	answe　をコンソールに表示する関数 f を定義する
+// 引数    : int 型の値を受け取る
+// 戻り値  : 引数の二つの値を足した値
+int f(int number, int number2) {
+	int answer = number + number2;
+	return answer;
+	
+}
