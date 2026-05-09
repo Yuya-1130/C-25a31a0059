875a4c9b406dd9b8ddefa1d61eac5186a7d250c9
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-09 10:48:55 +0900

    a

---
+++ jyugyou/jyugyou/file/fileA.cpp
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int f(int number, float value)
+{
+	int answer = number + (int)value;
+	printf("ŠÖ”‚†‚ªŒÄ‚Ño‚³‚ê‚Ü‚µ‚½Banswer‚Í[%d] ‚Å‚·\n", answer);
+
+	return answer;
+}
---
+++ jyugyou/jyugyou/kadai1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include "file/fileA.h"
+#include "file/fileC.h"
+
+
+
+int main()
+{
+	 f(1, 1.0f);
+	 f2();
+	
+
+	return 0;
+}
+
